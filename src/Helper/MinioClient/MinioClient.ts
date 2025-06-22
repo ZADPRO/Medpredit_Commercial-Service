b309bd69a98679e00220147373c429d30c4e7a76
ionhax-dev <thirukumara.d@zadroit.com>
2025-06-22 15:23:04 +0530

    ip changed

--- src/Helper/MinioClient/MinioClient.ts
+++ src/Helper/MinioClient/MinioClient.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const minioClient = new Client({
-  endPoint: process.env.MINIO_ENDPOINT || "192.168.1.67",
+  endPoint: process.env.MINIO_ENDPOINT || "192.168.1.112",
   port: parseInt(process.env.MINIO_PORT || "9000"),
   useSSL: process.env.MINIO_USE_SSL === "true",
   accessKey: process.env.MINIO_ACCESS_KEY || "SKKzNW1oGq2zRCkhlRuv",
