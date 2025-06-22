ca6d80d60e9ea270219bdf0ee3218307e62dde15
ionhax-dev <thirukumara.d@zadroit.com>
2025-06-22 15:25:53 +0530

    min io client

--- src/Helper/MinioClient/MinioClient.ts
+++ src/Helper/MinioClient/MinioClient.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export const minioClient = new Client({
   endPoint: process.env.MINIO_ENDPOINT || "192.168.1.112",
   port: parseInt(process.env.MINIO_PORT || "9000"),
-  useSSL: process.env.MINIO_USE_SSL === "true",
+  useSSL: process.env.MINIO_USE_SSL === "false",
   accessKey: process.env.MINIO_ACCESS_KEY || "SKKzNW1oGq2zRCkhlRuv",
   secretKey:
     process.env.MINIO_SECRET_KEY || "blevZGNgaJB5HimkC0X7Sigx7aqbytrecCP9BzP5",
