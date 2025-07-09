c46d9c3b34aafd5bdb0f6cc7118ec96360e12b6e
indumathi-ravi <indumathi123indumathi@gmail.com>
2025-07-09 15:49:56 +0530

    update env

--- src/Models/Commercial/CommercialQuery.ts
+++ src/Models/Commercial/CommercialQuery.ts
@@ -23,7 +23,7 @@
 //   "refSector" = $8,
 //   "activeStatus" = $9,
 //   "updatedBy" = $10
-  
+
 // WHERE "refUserId" = $11
 // RETURNING *;
 // `;
@@ -34,14 +34,14 @@
 //   "refAddress" = $2,
 //   "refDistrict" = $3,
 //   "refPincode" = $4
-  
+
 // WHERE "refUserId" = $5
 // RETURNING *;
 // `;
 
 // export const changeUserpostActiveQuery = `
 // UPDATE
-//   public."Users" 
+//   public."Users"
 // SET
 //   "activeStatus" = $1,
 //   "updatedAt" = $2,
@@ -58,7 +58,7 @@
 //   "headStatus" = $1,
 //   "updatedAt" = $2,
 //   "updatedBy" = $3
-//   WHERE 
+//   WHERE
 //   "refUserId" = $4
 // `;
 
@@ -314,7 +314,6 @@
 // LIMIT
 //   1;
 // `;
-
 
 export const getUserQuery = `
 SELECT
@@ -662,8 +661,170 @@ LIMIT
   1;
 `;
 
+export const getStressReportdataQuery = `
+SELECT
+  pt.*,
+  usd.*,
+  usv.*,
+  CASE
+    WHEN usv."refAction" = 'lessThanEqual'
+    AND pt."refPTScore"::integer <= CAST(usv."refValue" AS INTEGER) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'greaterThanEqual'
+    AND pt."refPTScore"::integer >= CAST(usv."refValue" AS INTEGER) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'rangeEqual'
+    AND pt."refPTScore"::integer BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS INTEGER) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS INTEGER)  THEN usv."refAnswerLabel"
+    ELSE NULL
+  END AS "calculatedAnswerLabel"
+FROM
+  public."refPatientTransaction" pt
+  LEFT JOIN public."refUserScoreDetail" usd ON CAST(usd."refPTId" AS INTEGER) = pt."refPTId"::integer
+  LEFT JOIN public."refUserScoreVerify" usv ON CAST(usv."refQCategoryId" AS INTEGER) = usd."refQCategoryId"::integer
+WHERE
+  pt."createdBy"::integer = $1
+  AND usd."refQCategoryId"::integer = $2
+  AND usv."refLanCode" = $3
+  AND (
+    (
+      usv."refAction" = 'lessThanEqual'
+      AND pt."refPTScore"::integer <= CAST(usv."refValue" AS INTEGER)
+    )
+    OR (
+      usv."refAction" = 'greaterThanEqual'
+      AND pt."refPTScore"::integer >= CAST(usv."refValue" AS INTEGER)
+    )
+    OR (
+      usv."refAction" = 'rangeEqual'
+      AND pt."refPTScore"::integer BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS INTEGER) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS INTEGER)
+    )
+  );
+`;
 
-export const checkUserSubscriptionQuery = `
+export const getDietaryReportdataQuery = `
+SELECT
+  pt.*,
+  usd.*,
+  usv.*,
+  CASE
+    WHEN usv."refAction" = 'equal'
+    AND pt."refPTScore"::integer = CAST(usv."refValue" AS INTEGER) THEN usv."refAnswerLabel"
+    ELSE NULL
+  END AS "calculatedAnswerLabel"
+FROM
+  public."refPatientTransaction" pt
+  LEFT JOIN public."refUserScoreDetail" usd ON CAST(usd."refPTId" AS INTEGER) = pt."refPTId"::integer
+  LEFT JOIN public."refUserScoreVerify" usv ON CAST(usv."refQCategoryId" AS INTEGER) = usd."refQCategoryId"::integer
+WHERE
+  pt."createdBy"::integer = $1
+  AND usd."refQCategoryId"::integer = $2
+  AND usv."refLanCode" = $3
+  AND usv."refAction" = 'equal'
+  AND pt."refPTScore"::integer = CAST(usv."refValue" AS INTEGER);
+`;
 
+export const getSleepReportdataQuery = `
+SELECT
+  pt.*,
+  usd.*,
+  usv.*,
+  CASE
+    WHEN usv."refAction" = 'equal'
+    AND pt."refPTScore"::integer = CAST(usv."refValue" AS INTEGER) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'rangeEqual'
+    AND pt."refPTScore"::integer BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS INTEGER) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS INTEGER)  THEN usv."refAnswerLabel"
+    ELSE NULL
+  END AS "calculatedAnswerLabel"
+FROM
+  public."refPatientTransaction" pt
+  LEFT JOIN public."refUserScoreDetail" usd ON CAST(usd."refPTId" AS INTEGER) = pt."refPTId"::integer
+  LEFT JOIN public."refUserScoreVerify" usv ON CAST(usv."refQCategoryId" AS INTEGER) = usd."refQCategoryId"::integer
+WHERE
+  pt."createdBy"::integer = $1
+  AND usd."refQCategoryId"::integer = $2
+  AND usv."refLanCode" = $3
+  AND (
+    (
+      usv."refAction" = 'equal'
+      AND pt."refPTScore"::integer = CAST(usv."refValue" AS INTEGER)
+    )
+    OR (
+      usv."refAction" = 'rangeEqual'
+      AND pt."refPTScore"::integer BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS INTEGER) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS INTEGER)
+    )
+  );
+`;
 
+export const getBMIReportdataQuery = `
+SELECT
+  pt.*,
+  usd.*,
+  usv.*,
+  CASE
+    WHEN usv."refAction" = 'lessThan'
+    AND pt."refPTScore"::numeric < CAST(usv."refValue" AS numeric) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'greaterThan'
+    AND pt."refPTScore"::numeric > CAST(usv."refValue" AS numeric) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'rangeEqual'
+    AND pt."refPTScore"::numeric BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS numeric) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS numeric)  THEN usv."refAnswerLabel"
+    ELSE NULL
+  END AS "calculatedAnswerLabel"
+FROM
+  public."refPatientTransaction" pt
+  LEFT JOIN public."refUserScoreDetail" usd ON CAST(usd."refPTId" AS INTEGER) = pt."refPTId"::integer
+  LEFT JOIN public."refUserScoreVerify" usv ON CAST(usv."refQCategoryId" AS INTEGER) = usd."refQCategoryId"::integer
+WHERE
+  pt."createdBy"::integer = $1
+  AND usd."refQCategoryId"::integer = $2
+  AND usv."refLanCode" = $3
+  AND (
+    (
+      usv."refAction" = 'lessThan'
+      AND pt."refPTScore"::numeric < CAST(usv."refValue" AS numeric)
+    )
+    OR (
+      usv."refAction" = 'greaterThan'
+      AND pt."refPTScore"::numeric > CAST(usv."refValue" AS numeric)
+    )
+    OR (
+      usv."refAction" = 'rangeEqual'
+      AND pt."refPTScore"::numeric BETWEEN CAST(SPLIT_PART(usv."refValue", ',', 1) AS numeric) AND CAST(SPLIT_PART(usv."refValue", ',', 2) AS numeric)
+    )
+  );
+`;
+
+export const getPhysicalActivityReportdataQuery = `
+SELECT
+  pt.*,
+  usd.*,
+  usv.*,
+  CASE
+    WHEN usv."refAction" = 'equal'
+    AND pt."refPTScore" = usv."refValue" THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'greaterThanEqual'
+    AND pt."refPTScore"::numeric >= CAST(usv."refValue" AS numeric) THEN usv."refAnswerLabel"
+    WHEN usv."refAction" = 'lessThan'
+    AND pt."refPTScore"::numeric < CAST(usv."refValue" AS numeric) THEN usv."refAnswerLabel"
+    ELSE NULL
+  END AS "calculatedAnswerLabel"
+FROM
+  public."refPatientTransaction" pt
+  LEFT JOIN public."refUserScoreDetail" usd ON CAST(usd."refPTId" AS INTEGER) = pt."refPTId"::integer
+  LEFT JOIN public."refUserScoreVerify" usv ON CAST(usv."refQCategoryId" AS INTEGER) = usd."refQCategoryId"::integer
+WHERE
+  pt."createdBy"::integer = $1
+  AND usd."refQCategoryId"::integer = $2
+  AND usv."refLanCode" = $3
+  AND (
+    (
+      usv."refAction" = 'equal'
+      AND pt."refPTScore" = usv."refValue"
+    )
+    OR (
+      usv."refAction" = 'greaterThanEqual'
+      AND pt."refPTScore"::numeric >= CAST(usv."refValue" AS numeric)
+    )
+    OR (
+      usv."refAction" = 'lessThan'
+      AND pt."refPTScore"::numeric < CAST(usv."refValue" AS numeric)
+    )
+  );
 `;
